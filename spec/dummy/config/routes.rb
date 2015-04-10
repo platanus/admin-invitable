d5667e48f25c1d9f10dc2c98571042e6a7d7f447
Leandro Segovia <ldlsegovia@gmail.com>
2015-04-10 12:51:07 -0300

    feat(): add home/index view to dummy app

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount AdminInvitable::Engine => "/admin_invitable"
+
+  get 'home/index'
+
+  root to: "home#index"
 end
